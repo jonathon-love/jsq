299930a6824109f7267432de01f785f39315b07a
Jonathon Love <jon@thon.cc>
2018-12-13 15:16:03 +1100

    Added IS t-test

---
+++ R/bttestis.b.R
@@ -0,0 +1,60 @@
+
+bttestISClass <- JAnalysis(
+    "bttestISClass",
+    inherit = bttestISBase,
+    private = list(
+        func=function() {
+            TTestBayesianIndependentSamples
+        },
+        .postInit=function(...) {
+            super$.postInit(...)
+            if (self$options$priorType == 'info' && self$options$test == 'mann')
+                stop('Informative priors are not supported with Mann-Whitney')
+        },
+        optionsMap=c(
+            vars = 'variables',
+            group = 'groupingVariable',
+            test = 'testStatistic',
+            `test:students` = 'Student',
+            `test:mann` = 'Wilcoxon',
+            mannN = 'wilcoxonSamplesNumber',
+            hypothesis = 'hypothesis',
+            `hypothesis:different` = 'groupsNotEqual',
+            `hypothesis:oneGreater` = 'groupOneGreater',
+            `hypothesis:twoGreater` = 'groupTwoGreater',
+            plotPP = 'plotPriorAndPosterior',
+            plotPPAddInfo = 'plotPriorAndPosteriorAdditionalInfo',
+            plotRobust = 'plotBayesFactorRobustness',
+            plotRobustAddInfo = 'plotBayesFactorRobustnessAdditionalInfo',
+            plotSeq = 'plotSequentialAnalysis',
+            plotSeqRobust = 'plotSequentialAnalysisRobustness',
+            plotDesc = 'descriptivesPlots',
+            plotDescCI = 'descriptivesPlotsCredibleInterval',
+            bfType = 'bayesFactorType',
+            desc = 'descriptives',
+            priorType = 'effectSizeStandardized',
+            `priorType:info` = 'informative',
+            `priorType:default` = 'default',
+            priorWidth = 'priorWidth',
+            infoShape = 'informativeStandardizedEffectSize',
+            infoCL = 'informativeCauchyLocation',
+            infoCS = 'informativeCauchyScale',
+            infoTL = 'informativeTLocation',
+            infoTS = 'informativeTScale',
+            infoTDf = 'informativeTDf',
+            infoNL = 'informativeNormalMean',
+            infoNS = 'informativeNormalStd',
+            plotWidth='plotWidth',
+            plotHeight='plotHeight',
+            missing='missingValues',
+            `missing:listwise`='excludeListwise')
+    ),
+    active=list(
+        joptions=function() {
+            options <- super$joptions
+            options$wilcoxText <- FALSE
+            options$descriptivesPlotsCredibleInterval <- options$descriptivesPlotsCredibleInterval / 100
+            options
+        }
+    )
+)
